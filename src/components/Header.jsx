b3d6b6e70ff4db2df9b59d4d9f64eb39f738f213
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2023-01-14 00:38:15 +0200

    adds data for all projects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Title from "./Title.jsx";
 const Header = () => (
-  <header className="flex items-center justify-center flex-col ">
-    <h1 className="text-4xl sm:text-5xl font-bold py-16 tracking-wider dark:text-white">Abozaid</h1>
+  <header className="flex items-center justify-center flex-col py-16 ">
+    <h1 className="text-4xl sm:text-5xl font-bold pb-16 tracking-wider dark:text-white">Abozaid</h1>
     <Title />
     <p className={`py-16 text-dimBlack dark:text-dimWhite max-w-[480px] text-center`}>
       I'm a computer science student, but I've been learning how to code since 2018. I've been passionate about web technologies enough to teach it to others by hosting web workshops for beginners. My ultimate goal is to make an impact in this domain, and I started doing so by teaching others, and improving my skills to make bigger and better projects from zero.
@@ -11,7 +11,7 @@ const Header = () => (
       <a href="https://docs.google.com/document/d/1Yd8hDpZ_lxWr0cFKhBdCqIMOORh3FEKgoJzdE1n9_70/edit?usp=sharing" target="_blank">
         <button className="bg-white text-dimBlack font-medium px-8 rounded-[10px] py-3 text-lg">Resume</button>
       </a>
-      <div className="absolute -z-10 inset-0 rounded-[10px] w-full h-full blue-pink-gradient blur-sm"></div>
+      <div className="absolute -z-10 inset-0 rounded-[10px] w-full h-full blue-pink-gradient blur-[7px]"></div>
     </div>
   </header>
 );
---
+++ src/data/index.js
@@ -0,0 +1,150 @@
+import devvio from "../assets/devvio.png"
+import movique from "../assets/movique.png"
+import loremBox from "../assets/loremBox.png"
+import todoApp from "../assets/todoApp.png"
+import aimHelper from "../assets/aimHelper.png"
+import passwordGenerator from "../assets/passwordGenerator.png"
+import liveUserFilter from "../assets/liveUserFilter.png"
+import goodFastCheap from "../assets/goodFastCheap.png"
+import githubProfileFinder from "../assets/githubProfileFinder.png"
+import weatherApp from "../assets/weatherApp.png"
+import quizCreator from "../assets/quizCreator.png"
+import typingSpeedTest from "../assets/typingSpeedTest.png"
+import pomodoroApp from "../assets/pomodoroApp.png"
+import oldPortfolio from "../assets/oldPortfolio.png"
+import AiTicTacToe from "../assets/AiTicTacToe.png"
+import simonGame from "../assets/simonGame.png"
+
+const projects = [
+  {
+    id:1,
+    name:"Devvio",
+    props:["React","Tailwind","2023"],
+    img:devvio,
+    type:"website",
+    link:"https://github.com/AhmedAbozaid9/Devvio"
+  },
+  {
+    id:2,
+    name:"Movique",
+    props:["React","Firebase","2022"],
+    img:movique,
+    type:"website",
+    link:"https://github.com/AhmedAbozaid9/movique"
+  },
+  {
+    id:3,
+    name:"Lorem Box",
+    props:["React","Sass","2022"],
+    img:loremBox,
+    type:"website",
+    link:"https://github.com/AhmedAbozaid9/lorem_box",
+  },
+  {
+    id:4,
+    name:"Todo App",
+    props:["JavaScript","React Native","2023"],
+    img:todoApp,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/Todo_App",
+  },
+  {
+    id:5,
+    name:"Password Generator",
+    props:["React","Tailwind","2022"],
+    img:passwordGenerator,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/Password_generator",
+  },
+  {
+    id:6,
+    name:"Live User Filter",
+    props:["JavaScript","React","2022"],
+    img:liveUserFilter,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/React_live_user_filter",
+  },
+  {
+    id:7,
+    name:"Github Profile Finder",
+    props:["JavaScript","React","2022"],
+    img:githubProfileFinder,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/React-github-profile-finder",
+  },
+  {
+    id:8,
+    name:"Good Fast & Cheap",
+    props:["React","Redux","2022"],
+    img:goodFastCheap,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/redux-good-fast-cheap",
+  },
+  {
+    id:9,
+    name:"Weather App",
+    props:["JavaScript","React","2022"],
+    img:weatherApp,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/React-weather-app",
+  },
+  {
+    id:10,
+    name:"Aim Helper",
+    props:["JavaScript","JQuery","2021"],
+    img:aimHelper,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/Aim_helper",
+  },
+  {
+    id:11,
+    name:"Quiz Creator",
+    props:["HTML","JavaScript","2021"],
+    img:quizCreator,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/Quiz-creator",
+  },
+  {
+    id:12,
+    name:"Typing Speed Test",
+    props:["HTML","JavaScript","2021"],
+    img:typingSpeedTest,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/typing-speed-test",
+  },
+  {
+    id:13,
+    name:"Pomodoro App",
+    props:["HTML","JavaScript","2021"],
+    img:pomodoroApp,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/pomodoro-app",
+  },
+  {
+    id:14,
+    name:"Old Portfolio",
+    props:["HTML","JavaScript","2021"],
+    img:oldPortfolio,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/old-portfolio",
+  },
+  {
+    id:15,
+    name:"AI Tic Tac Toe",
+    props:["Python","PyGame","2021"],
+    img:AiTicTacToe,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/AI-tick-tack-toe",
+  },
+  {
+    id:16,
+    name:"Simon Game",
+    props:["JavaScript","JQuery","2020"],
+    img:simonGame,
+    type:"app",
+    link:"https://github.com/AhmedAbozaid9/Simon-Game",
+  },
+]
+
+
+export default projects
