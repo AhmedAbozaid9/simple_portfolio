c429be84b160507f9e2adf65f7d7c01de32ce029
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2023-01-17 20:53:24 +0200

    adds style to inputs when focused

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => (
       className="pt-5 w-full flex flex-col items-center justify-center"
     >
       <div className="lg:w-[1000px] w-full flex flex-col items-center justify-center">
-        <input required type="text" name="name" placeholder="Name" className="outline-0 w-full p-5 my-2 rounded-[10px] border-2 border-neutral-400 dark:border-white dark:bg-neutral-900"/>
-        <input required type="email" name="email" placeholder="Email" className="outline-0 w-full p-5 my-2 rounded-[10px] border-2 border-neutral-400 dark:border-white dark:bg-neutral-900"/>
-        <textarea required name="message" id="" cols="30" rows="10" className="outline-0 w-full  p-5 my-2 rounded-[10px] border-2 border-neutral-400 dark:border-white min-h-[100px] dark:bg-neutral-900" placeholder="Message"></textarea>
+        <input required type="text" name="name" placeholder="Name" className="outline-0 w-full p-5 my-2 rounded-[10px] focus:border-neonBlue focus:dark:border-neonPink border-2 border-neutral-400 dark:border-white dark:bg-neutral-900"/>
+        <input required type="email" name="email" placeholder="Email" className="outline-0 w-full p-5 my-2 rounded-[10px] border-2 focus:border-neonBlue focus:dark:border-neonPink border-neutral-400 dark:border-white dark:bg-neutral-900"/>
+        <textarea required name="message" id="" cols="30" rows="10" className="outline-0 w-full  p-5 my-2 rounded-[10px] focus:border-neonBlue focus:dark:border-neonPink border-2 border-neutral-400 dark:border-white min-h-[100px] dark:bg-neutral-900" placeholder="Message"></textarea>
         <input type="submit" value="Contact Me" className="text-lg font-medium cursor-pointer py-4 px-8 rounded-[12px] text-white button-gradient self-start mt-8 drop-shadow-lg"/>
       </div>
     </form>
