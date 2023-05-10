0c852d6e1f1c1321fb97a447016d799e17fab1c4
ahmed <ahmedabozaid493@gmail.com>
2023-05-10 22:27:08 +0300

    changes the resume link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
 import Title from "./Title.jsx";
 const Header = () => (
   <header className="flex items-center justify-center flex-col py-16 ">
-    <h1 className="text-4xl sm:text-5xl font-bold pb-16 tracking-wider dark:text-white">Abozaid</h1>
+    <h1 className="text-4xl sm:text-5xl font-bold pb-16 tracking-wider dark:text-white">
+      Abozaid
+    </h1>
     <Title />
-    <p className={"py-16 text-dimBlack dark:text-dimWhite max-w-[500px] text-center tracking-wide"}>
-      I'm a computer science student, but I've been learning how to code since 2018. I've been passionate about web technologies enough to teach it to others by hosting web workshops for beginners. My ultimate goal is to make an impact in this domain, and I started doing so by teaching others, and improving my skills to make bigger and better projects from zero.
+    <p
+      className={
+        "py-16 text-dimBlack dark:text-dimWhite max-w-[500px] text-center tracking-wide"
+      }
+    >
+      I'm a computer science student, but I've been learning how to code since
+      2018. I've been passionate about web technologies enough to teach it to
+      others by hosting web workshops for beginners. My ultimate goal is to make
+      an impact in this domain, and I started doing so by teaching others, and
+      improving my skills to make bigger and better projects from zero.
     </p>
     <div className="relative">
-      <a href="https://docs.google.com/document/d/1Yd8hDpZ_lxWr0cFKhBdCqIMOORh3FEKgoJzdE1n9_70/edit?usp=sharing" target="_blank">
-        <button className="bg-white text-dimBlack font-medium px-8 rounded-[10px] py-3 text-lg">Resume</button>
+      <a
+        href="https://docs.google.com/document/d/1umPKNLRVEk0Ar_viTRFBK72Jhcg5SUV4MTx8y88elBQ/edit?usp=sharing"
+        target="_blank"
+      >
+        <button className="bg-white text-dimBlack font-medium px-8 rounded-[10px] py-3 text-lg">
+          Resume
+        </button>
       </a>
       <div className="absolute -z-10 inset-0 rounded-[10px] w-full h-full button-gradient blur-[7px] dark:blur-lg"></div>
     </div>
